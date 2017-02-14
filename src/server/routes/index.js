4f3e3e9b2650a2eee5f2d2c68aff80c5c1f24332
@soyjavi <javi.jimenez.villar@gmail.com>
2017-02-14 23:50:00 +0100

    Fix views folder

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,17 @@
+import path from 'path';
 import express from 'express';
 import home from './home';
 import job from './job';
 import post from './post';
+import reactRouter from './reactRouter';
 
 const app = express();
 
-// app.use('/', home);
+app.set('views', path.resolve('.', 'src/server/views'));
+
+app.use('/', home);
 app.use('/', job);
-// app.use('/', post);
+app.use('/', post);
+app.use('/', reactRouter);
 
 export default app;
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,7 @@
-import path from 'path';
 import Express from 'express';
 import C from '../shared/constants';
 import { Error, HotReload, Log, Stats, Webpack } from './middlewares';
 import routes from './routes';
-import reactRouter from './routes/reactRouter'
 
 const environment = process.env.NODE_ENV || C.environment.development;
 const port = process.env.PORT || 3000;
@@ -11,23 +9,20 @@ const app = Express();
 
 // -- Setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-// app.set('views', path.resolve('.', 'src/server/views')); // @TODO: 🤔 Pass config between routers
 
 // -- Session configuration
 // @TODO
 
 // -- Middleware and routes
 app.use('/static', Express.static('static'));
-app.use(Stats);
 app.use(Webpack);
+if (environment !== C.environment.development) app.use(Stats);
 if (environment === C.environment.development) {
   app.use(Log);
   app.use(HotReload);
 }
 app.use(Error);
 app.use('/', routes);
-app.use(reactRouter);
 
 // -- Start Server
 app.listen(port, (error) => {
