c4a8516e7ffea4cda221dba9bd8ddc507dfec7a3
@soyjavi <javi.jimenez.villar@gmail.com>
2017-02-17 15:29:49 +0100

    #Improvemente first schema for Order model.

--- src/server/models/offer/consolidate.js
+++ src/server/models/offer/consolidate.js
@@ -2,6 +2,11 @@ export default state => ({
 
   consolidate(provider, providerId, data = {}) {
     const query = { provider, providerId };
+    const offerInStore = state.find({ query });
+
+    if (offerInStore) {
+      //@TODO: Notify to our telegram group
+    }
 
     return state.update({ query, data, upsert: true });
   },
--- src/server/models/offer/offer.js
+++ src/server/models/offer/offer.js
@@ -2,7 +2,27 @@ import Model from '../base';
 import consolidate from './consolidate';
 
 export default (() => {
-  const state = new Model({ file: 'db', key: 'offers' });
+  const state = new Model({
+    file: 'db',
+    key: 'offers',
+    schema: {
+      category: undefined,
+      position: undefined,
+      url: undefined,
+      remote: false,
+      location: undefined,
+
+      company: undefined,
+      companyUrl: undefined,
+      companyAbout: undefined,
+      companyImage: undefined,
+
+      text: undefined,
+      state: 'draft',
+      highlight: false,
+      createdAt: undefined,
+    },
+  });
 
   return Object.assign(
     state,
