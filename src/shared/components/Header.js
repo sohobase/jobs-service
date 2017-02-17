a5d6e6f245feadfa0c217a1ff50ea1bb6cf70949
@soyjavi <javi.jimenez.villar@gmail.com>
2017-02-17 19:11:48 +0100

    Include <Button> on <Header>

--- src/shared/components/Header.js
+++ src/shared/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Button from './Button';
 import style from './Header.css';
 
 export default () => (
   <header className={style.header}>
     <div className={style.wrapper}>
       <Link to="/">
-        <img src="/static/img/logo.png" className={style.brand} />
+        <img src="/static/img/logo.png" className={style.brand} alt="" />
       </Link>
       <nav className={style.navigation}>
         <Link to="/help">Ask us anything</Link>
         <Link to="/job/1234">Example Job</Link>
-        <Link to="/post">Post new offer</Link>
+        <Button caption="Post new offer" accent />
       </nav>
     </div>
   </header>
