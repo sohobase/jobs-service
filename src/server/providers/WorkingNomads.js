83eab1395e5429035617b8b36a3230b92c788396
rubenmoya <rmoyarodriguez@gmail.com>
2017-02-18 02:15:17 +0100

    #feature add WorkingNomads provider

---
+++ src/server/providers/WorkingNomads.js
@@ -0,0 +1,43 @@
+import https from 'https';
+import telegramBot from '../services/telegramBot';
+import { Offer } from '../models';
+
+const callback = (response) => {
+  let str = '';
+
+  response.on('data', (chunk) => {
+    str += chunk;
+  });
+
+  response.on('end', () => {
+    const result = JSON.parse(str).hits.hits;
+
+    result.map(({ _source }) => {
+      const { id, apply_url, category_name, company, description, location_base, pub_date, tags, title } = _source;
+      if (id) {
+        Offer.consolidate('workingnomads', id, {
+          category: category_name,
+          position: title,
+          url: apply_url,
+          remote: !location_base, // eslint-disable-line camelcase
+          location: location_base,
+
+          company,
+
+          text: description,
+          state: 'imported',
+          skills: tags,
+
+          createdAt: pub_date,
+        });
+      }
+      return id;
+    });
+    telegramBot('⚙️ /cron/workingnomads : 🏁');
+  });
+};
+
+https
+  .get('https://www.workingnomads.co/jobsapi/job/_search?sort=pub_date:desc&size=100', callback)
+  .on('error', error => telegramBot(`⚙️ /cron/workingnomads : 🚨 (${error})`))
+  .end();
