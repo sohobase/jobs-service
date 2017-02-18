0110e8e11203024aa44564973fa409a114af486c
@soyjavi <javi.jimenez.villar@gmail.com>
2017-02-18 13:46:07 +0100

    scaffold <JobListItem>

--- src/shared/components/JobListItem.js
+++ src/shared/components/JobListItem.js
@@ -1,14 +1,46 @@
+import classnames from 'classnames';
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { ProviderDate } from '../providers';
 import style from './JobListItem.css';
 
-const JobListItem = ({ name }) => (
-  <li className={style.joblistitem}>
-    {name}
-  </li>
-);
+const JobListItem = (props = {}) => {
+  const {
+    id,
+    category,
+    createdAt = (new Date()),
+    company,
+    companyImage,
+    position,
+  } = props;
+
+  const isNew = ProviderDate.inLast24Hours(createdAt);
+  const classes = classnames(style.joblistitem, style[category]);
+
+  console.log(isNew);
+
+  return (
+    <Link to={`/job/${id}`}>
+      <li className={classes}>
+        { isNew && <abbr className={style.new}>NEW</abbr> }
+        { companyImage && <img src={companyImage} alt={company} className={style.image} /> }
+        <div className={style.info}>
+          <small className={style.company}>{company}</small>
+          <strong className={style.position}>{position}</strong>
+        </div>
+        <small className={style.date}>{ProviderDate.ago(createdAt)}</small>
+      </li>
+    </Link>
+  );
+};
 
 JobListItem.propTypes = {
-  name: PropTypes.string,
+  category: PropTypes.string,
+  company: PropTypes.string,
+  companyImage: PropTypes.string,
+  id: PropTypes.string,
+  position: PropTypes.string,
+  createdAt: PropTypes.any,
 };
 
 export default JobListItem;
