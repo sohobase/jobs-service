3c4ee29700b845f0ade1560d866dc8ef3bf867ae
rubenmoya <rmoyarodriguez@gmail.com>
2017-02-18 17:15:15 +0100

    #refactor remove serverRendering

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -1,13 +1,41 @@
 import path from 'path';
 import Express from 'express';
-import serverRendering from '../services/serverRendering';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import NotFoundPage from '../../shared/components/NotFoundPage';
+import routes from '../../shared/routes';
+import storeService from '../services/storeService';
 
 const app = Express();
+const handleRequest = (req, res) => {
+  match({ routes, location: req.originalUrl }, (error, redirect, routerProps) => {
+    if (error) return res.status(500).send(error.message);
+    if (redirect) return res.redirect(302, redirect.pathname + redirect.search);
+
+    let markup;
+    const store = storeService(req.originalUrl, req.params);
+    if (routerProps) {
+      const renderProps = {
+        ...routerProps,
+        createElement: (Component, props) => (
+          <Component {...props} dataSource={store} />
+        ),
+      };
+
+      markup = renderToString(<RouterContext {...renderProps} />);
+    } else {
+      markup = renderToString(<NotFoundPage />);
+      res.status(404);
+    }
+
+    return res.render('index', { markup, store });
+  });
+};
 
 app.set('views', path.resolve('.', 'src/server/views'));
 app.use('/static', Express.static(path.resolve('.', 'public')));
-app.use('/job/:id', serverRendering);
-app.use('/', serverRendering);
-
+app.use('/job/:id', handleRequest);
+app.use('/', handleRequest);
 
 export default app;
